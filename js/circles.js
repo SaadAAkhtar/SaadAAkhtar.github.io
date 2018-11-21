7a31891beedf3be2709d3cfba8a73f5b37e09a3a
sakhtar30 <saakhta@gmail.com>
2018-11-21 17:51:49 -0500

    Circles complete

--- js/circles.js
+++ js/circles.js
@@ -17,63 +17,63 @@ function check(){
 	var question15 = document.circlespractice.Circles15.value;
 	var correct = 0;
 	
-	if (question1 == "") {
+	if (question1 == "A") {
 		correct++;
 	}
 	
-	if (question2 == "") {
+	if (question2 == "A") {
 		correct++;
 	}
 	
-	if (question3 == "") {
+	if (question3 == "32") {
 		correct++;
 	}
 	
-	if (question4 == "") {
+	if (question4 == "5/8" || question4 == ".625" || question4 == "0.625") {
 		correct++;
 	}
 	
-	if (question5 == "") {
+	if (question5 == "C") {
 		correct++;
 	}
 	
-	if (question6 == "") {
+	if (question6 == "29" || question6 == "30" || question6 == "31" || question6 == "32" || question6 == "33" || question6 == "34") {
 		correct++;
 	}
 	
-	if (question7 == "") {
+	if (question7 == "A") {
 		correct++;
 	}
 	
-	if (question8 == "") {
+	if (question8 == "B") {
 		correct++;
 	}
 	
-	if (question9 == "") {
+	if (question9 == "80") {
 		correct++;
 	}
 	
-	if (question10 == "") {
+	if (question10 == "1/6" || question10 == ".166" || question10 == ".167" || question10 == "0.166" || question10 == "0.167") {
 		correct++;
 	}
 	
-	if (question11 == "") {
+	if (question11 == "A") {
 		correct++;
 	}
 	
-	if (question12 == "") {
+	if (question12 == "D") {
 		correct++;
 	}
 	
-	if (question13 == "") {
+	if (question13 == "5/18" || question13 == ".277" || question13 == ".278" || question13 == "0.277" || question13 == "0.278") {
 		correct++;
 	}
 	
-	if (question14 == "") {
+	if (question14 == "A") {
 		correct++;
 	}
 	
-	if (question15 == "") {
+	if (question15 == "144") {
 		correct++;
 	}
 	
---
+++ js/statisticsandprobability.js
@@ -0,0 +1,268 @@
+function check(){
+	
+	var question1 = document.statisticsandprobabilitypractice.StatisticsAndProbability1.value;
+	var question2 = document.statisticsandprobabilitypractice.StatisticsAndProbability2.value;
+	var question3 = document.statisticsandprobabilitypractice.StatisticsAndProbability3.value;
+	var question4 = document.statisticsandprobabilitypractice.StatisticsAndProbability4.value;
+	var question5 = document.statisticsandprobabilitypractice.StatisticsAndProbability5.value;
+	var question6 = document.statisticsandprobabilitypractice.StatisticsAndProbability6.value;
+	var question7 = document.statisticsandprobabilitypractice.StatisticsAndProbability7.value;
+	var question8 = document.statisticsandprobabilitypractice.StatisticsAndProbability8.value;
+	var question9 = document.statisticsandprobabilitypractice.StatisticsAndProbability9.value;
+	var question10 = document.statisticsandprobabilitypractice.StatisticsAndProbability10.value;
+	var question11 = document.statisticsandprobabilitypractice.StatisticsAndProbability11.value;
+	var question12 = document.statisticsandprobabilitypractice.StatisticsAndProbability12.value;
+	var question13 = document.statisticsandprobabilitypractice.StatisticsAndProbability13.value;
+	var question14 = document.statisticsandprobabilitypractice.StatisticsAndProbability14.value;
+	var question15 = document.statisticsandprobabilitypractice.StatisticsAndProbability15.value;
+	var question16 = document.statisticsandprobabilitypractice.StatisticsAndProbability16.value;
+	var question17 = document.statisticsandprobabilitypractice.StatisticsAndProbability17.value;
+	var question18 = document.statisticsandprobabilitypractice.StatisticsAndProbability18.value;
+	var question19 = document.statisticsandprobabilitypractice.StatisticsAndProbability19.value;
+	var question20 = document.statisticsandprobabilitypractice.StatisticsAndProbability20.value;
+	var question21 = document.statisticsandprobabilitypractice.StatisticsAndProbability21.value;
+	var question22 = document.statisticsandprobabilitypractice.StatisticsAndProbability22.value;
+	var question23 = document.statisticsandprobabilitypractice.StatisticsAndProbability23.value;
+	var question24 = document.statisticsandprobabilitypractice.StatisticsAndProbability24.value;
+	var question25 = document.statisticsandprobabilitypractice.StatisticsAndProbability25.value;
+	var question26 = document.statisticsandprobabilitypractice.StatisticsAndProbability26.value;
+	var question27 = document.statisticsandprobabilitypractice.StatisticsAndProbability27.value;
+	var question28 = document.statisticsandprobabilitypractice.StatisticsAndProbability28.value;
+	var question29 = document.statisticsandprobabilitypractice.StatisticsAndProbability29.value;
+	var question30 = document.statisticsandprobabilitypractice.StatisticsAndProbability30.value;
+	var question31 = document.statisticsandprobabilitypractice.StatisticsAndProbability31.value;
+	var question32 = document.statisticsandprobabilitypractice.StatisticsAndProbability32.value;
+	var question33 = document.statisticsandprobabilitypractice.StatisticsAndProbability33.value;
+	var question34 = document.statisticsandprobabilitypractice.StatisticsAndProbability34.value;
+	var question35 = document.statisticsandprobabilitypractice.StatisticsAndProbability35.value;
+	var question36 = document.statisticsandprobabilitypractice.StatisticsAndProbability36.value;
+	var question37 = document.statisticsandprobabilitypractice.StatisticsAndProbability37.value;
+	var question38 = document.statisticsandprobabilitypractice.StatisticsAndProbability38.value;
+	var question39 = document.statisticsandprobabilitypractice.StatisticsAndProbability39.value;
+	var question40 = document.statisticsandprobabilitypractice.StatisticsAndProbability40.value;
+	var question41 = document.statisticsandprobabilitypractice.StatisticsAndProbability41.value;
+	var question42 = document.statisticsandprobabilitypractice.StatisticsAndProbability42.value;
+	var question43 = document.statisticsandprobabilitypractice.StatisticsAndProbability43.value;
+	var question44 = document.statisticsandprobabilitypractice.StatisticsAndProbability44.value;
+	var question45 = document.statisticsandprobabilitypractice.StatisticsAndProbability45.value;
+	var question46 = document.statisticsandprobabilitypractice.StatisticsAndProbability46.value;
+	var question47 = document.statisticsandprobabilitypractice.StatisticsAndProbability47.value;
+	var question48 = document.statisticsandprobabilitypractice.StatisticsAndProbability48.value;
+	var question49 = document.statisticsandprobabilitypractice.StatisticsAndProbability49.value;
+	var question50 = document.statisticsandprobabilitypractice.StatisticsAndProbability50.value;
+	var question51 = document.statisticsandprobabilitypractice.StatisticsAndProbability51.value;
+	var question52 = document.statisticsandprobabilitypractice.StatisticsAndProbability52.value;
+	var correct = 0;
+	
+	if (question1 == "") {
+		correct++;
+	}
+	
+	if (question2 == "") {
+		correct++;
+	}
+	
+	if (question3 == "") {
+		correct++;
+	}
+	
+	if (question4 == "") {
+		correct++;
+	}
+	
+	if (question5 == "") {
+		correct++;
+	}
+	
+	if (question6 == "") {
+		correct++;
+	}
+	
+	if (question7 == "") {
+		correct++;
+	}
+	
+	if (question8 == "") {
+		correct++;
+	}
+	
+	if (question9 == "") {
+		correct++;
+	}
+	
+	if (question10 == "") {
+		correct++;
+	}
+	
+	if (question11 == "") {
+		correct++;
+	}
+	
+	if (question12 == "") {
+		correct++;
+	}
+	
+	if (question13 == "") {
+		correct++;
+	}
+	
+	if (question14 == "") {
+		correct++;
+	}
+	
+	if (question15 == "") {
+		correct++;
+	}
+	
+	if (question16 == "") {
+		correct++;
+	}
+	
+	if (question17 == "") {
+		correct++;
+	}
+	
+	if (question18 == "") {
+		correct++;
+	}
+	
+	if (question19 == "") {
+		correct++;
+	}
+	
+	if (question20 == "") {
+		correct++;
+	}
+	
+	if (question21 == "") {
+		correct++;
+	}
+	
+	if (question22 == "") {
+		correct++;
+	}
+	
+	if (question23 == "") {
+		correct++;
+	}
+	
+	if (question24 == "") {
+		correct++;
+	}
+	
+	if (question25 == "") {
+		correct++;
+	}
+	
+	if (question26 == "") {
+		correct++;
+	}
+	
+	if (question27 == "") {
+		correct++;
+	}
+	
+	if (question28 == "") {
+		correct++;
+	}
+	
+	if (question29 == "") {
+		correct++;
+	}
+	
+	if (question30 == "") {
+		correct++;
+	}
+	
+	if (question31 == "") {
+		correct++;
+	}
+	
+	if (question32 == "") {
+		correct++;
+	}
+	
+	if (question33 == "") {
+		correct++;
+	}
+	
+	if (question34 == "") {
+		correct++;
+	}
+	
+	if (question35 == "") {
+		correct++;
+	}
+	
+	if (question36 == "") {
+		correct++;
+	}
+	
+	if (question37 == "") {
+		correct++;
+	}
+	
+	if (question38 == "") {
+		correct++;
+	}
+	
+	if (question39 == "") {
+		correct++;
+	}
+	
+	if (question40 == "") {
+		correct++;
+	}
+	
+	if (question41 == "") {
+		correct++;
+	}
+	
+	if (question42 == "") {
+		correct++;
+	}
+	
+	if (question43 == "") {
+		correct++;
+	}
+	
+	if (question44 == "") {
+		correct++;
+	}
+	
+	if (question45 == "") {
+		correct++;
+	}
+	
+	if (question46 == "") {
+		correct++;
+	}
+	
+	if (question47 == "") {
+		correct++;
+	}
+	
+	if (question48 == "") {
+		correct++;
+	}
+	
+	if (question49 == "") {
+		correct++;
+	}
+	
+	if (question50 == "") {
+		correct++;
+	}
+	
+	if (question51 == "") {
+		correct++;
+	}
+	
+	if (question52 == "") {
+		correct++;
+	}
+	
+	document.getElementById("after_submit").style.visibility = "visible";
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct!";
+	
+}
