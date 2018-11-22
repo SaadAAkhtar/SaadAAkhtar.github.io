f59dbe8520b64a45d3886e02b1a677e6478c4530
sakhtar30 <saakhta@gmail.com>
2018-11-21 23:45:23 -0500

    Set up Systems of Equations for form completion

---
+++ js/systemsofequations.js
@@ -0,0 +1,292 @@
+function check(){
+	
+	var question1 = document.systemsofequationspractice.SystemsOfEquations1.value;
+	var question2 = document.systemsofequationspractice.SystemsOfEquations2.value;
+	var question3 = document.systemsofequationspractice.SystemsOfEquations3.value;
+	var question4 = document.systemsofequationspractice.SystemsOfEquations4.value;
+	var question5 = document.systemsofequationspractice.SystemsOfEquations5.value;
+	var question6 = document.systemsofequationspractice.SystemsOfEquations6.value;
+	var question7 = document.systemsofequationspractice.SystemsOfEquations7.value;
+	var question8 = document.systemsofequationspractice.SystemsOfEquations8.value;
+	var question9 = document.systemsofequationspractice.SystemsOfEquations9.value;
+	var question10 = document.systemsofequationspractice.SystemsOfEquations10.value;
+	var question11 = document.systemsofequationspractice.SystemsOfEquations11.value;
+	var question12 = document.systemsofequationspractice.SystemsOfEquations12.value;
+	var question13 = document.systemsofequationspractice.SystemsOfEquations13.value;
+	var question14 = document.systemsofequationspractice.SystemsOfEquations14.value;
+	var question15 = document.systemsofequationspractice.SystemsOfEquations15.value;
+	var question16 = document.systemsofequationspractice.SystemsOfEquations16.value;
+	var question17 = document.systemsofequationspractice.SystemsOfEquations17.value;
+	var question18 = document.systemsofequationspractice.SystemsOfEquations18.value;
+	var question19 = document.systemsofequationspractice.SystemsOfEquations19.value;
+	var question20 = document.systemsofequationspractice.SystemsOfEquations20.value;
+	var question21 = document.systemsofequationspractice.SystemsOfEquations21.value;
+	var question22 = document.systemsofequationspractice.SystemsOfEquations22.value;
+	var question23 = document.systemsofequationspractice.SystemsOfEquations23.value;
+	var question24 = document.systemsofequationspractice.SystemsOfEquations24.value;
+	var question25 = document.systemsofequationspractice.SystemsOfEquations25.value;
+	var question26 = document.systemsofequationspractice.SystemsOfEquations26.value;
+	var question27 = document.systemsofequationspractice.SystemsOfEquations27.value;
+	var question28 = document.systemsofequationspractice.SystemsOfEquations28.value;
+	var question29 = document.systemsofequationspractice.SystemsOfEquations29.value;
+	var question30 = document.systemsofequationspractice.SystemsOfEquations30.value;
+	var question31 = document.systemsofequationspractice.SystemsOfEquations31.value;
+	var question32 = document.systemsofequationspractice.SystemsOfEquations32.value;
+	var question33 = document.systemsofequationspractice.SystemsOfEquations33.value;
+	var question34 = document.systemsofequationspractice.SystemsOfEquations34.value;
+	var question35 = document.systemsofequationspractice.SystemsOfEquations35.value;
+	var question36 = document.systemsofequationspractice.SystemsOfEquations36.value;
+	var question37 = document.systemsofequationspractice.SystemsOfEquations37.value;
+	var question38 = document.systemsofequationspractice.SystemsOfEquations38.value;
+	var question39 = document.systemsofequationspractice.SystemsOfEquations39.value;
+	var question40 = document.systemsofequationspractice.SystemsOfEquations40.value;
+	var question41 = document.systemsofequationspractice.SystemsOfEquations41.value;
+	var question42 = document.systemsofequationspractice.SystemsOfEquations42.value;
+	var question43 = document.systemsofequationspractice.SystemsOfEquations43.value;
+	var question44 = document.systemsofequationspractice.SystemsOfEquations44.value;
+	var question45 = document.systemsofequationspractice.SystemsOfEquations45.value;
+	var question46 = document.systemsofequationspractice.SystemsOfEquations46.value;
+	var question47 = document.systemsofequationspractice.SystemsOfEquations47.value;
+	var question48 = document.systemsofequationspractice.SystemsOfEquations48.value;
+	var question49 = document.systemsofequationspractice.SystemsOfEquations49.value;
+	var question50 = document.systemsofequationspractice.SystemsOfEquations50.value;
+	var question51 = document.systemsofequationspractice.SystemsOfEquations51.value;
+	var question52 = document.systemsofequationspractice.SystemsOfEquations52.value;
+	var question53 = document.systemsofequationspractice.SystemsOfEquations53.value;
+	var question54 = document.systemsofequationspractice.SystemsOfEquations54.value;
+	var question55 = document.systemsofequationspractice.SystemsOfEquations55.value;
+	var question56 = document.systemsofequationspractice.SystemsOfEquations56.value;
+	var correct = 0;
+	
+	if (question1 == "") {
+		correct++;
+	}
+	
+	if (question2 == "") {
+		correct++;
+	}
+	
+	if (question3 == "") {
+		correct++;
+	}
+	
+	if (question4 == "") {
+		correct++;
+	}
+	
+	if (question5 == "") {
+		correct++;
+	}
+	
+	if (question6 == "") {
+		correct++;
+	}
+	
+	if (question7 == "") {
+		correct++;
+	}
+	
+	if (question8 == "") {
+		correct++;
+	}
+	
+	if (question9 == "") {
+		correct++;
+	}
+	
+	if (question10 == "") {
+		correct++;
+	}
+	
+	if (question11 == "") {
+		correct++;
+	}
+	
+	if (question12 == "") {
+		correct++;
+	}
+	
+	if (question13 == "") {
+		correct++;
+	}
+	
+	if (question14 == "") {
+		correct++;
+	}
+	
+	if (question15 == "") {
+		correct++;
+	}
+	
+	if (question16 == "") {
+		correct++;
+	}
+	
+	if (question17 == "") {
+		correct++;
+	}
+	
+	if (question18 == "") {
+		correct++;
+	}
+	
+	if (question19 == "") {
+		correct++;
+	}
+	
+	if (question20 == "") {
+		correct++;
+	}
+	
+	if (question21 == "") {
+		correct++;
+	}
+	
+	if (question22 == "") {
+		correct++;
+	}
+	
+	if (question23 == "") {
+		correct++;
+	}
+	
+	if (question24 == "") {
+		correct++;
+	}
+	
+	if (question25 == "") {
+		correct++;
+	}
+	
+	if (question26 == "") {
+		correct++;
+	}
+	
+	if (question27 == "") {
+		correct++;
+	}
+	
+	if (question28 == "") {
+		correct++;
+	}
+	
+	if (question29 == "") {
+		correct++;
+	}
+	
+	if (question30 == "") {
+		correct++;
+	}
+	
+	if (question31 == "") {
+		correct++;
+	}
+	
+	if (question32 == "") {
+		correct++;
+	}
+	
+	if (question33 == "") {
+		correct++;
+	}
+	
+	if (question34 == "") {
+		correct++;
+	}
+	
+	if (question35 == "") {
+		correct++;
+	}
+	
+	if (question36 == "") {
+		correct++;
+	}
+	
+	if (question37 == "") {
+		correct++;
+	}
+	
+	if (question38 == "") {
+		correct++;
+	}
+	
+	if (question39 == "") {
+		correct++;
+	}
+	
+	if (question40 == "") {
+		correct++;
+	}
+	
+	if (question41 == "") {
+		correct++;
+	}
+	
+	if (question42 == "") {
+		correct++;
+	}
+	
+	if (question43 == "") {
+		correct++;
+	}
+	
+	if (question44 == "") {
+		correct++;
+	}
+	
+	if (question45 == "") {
+		correct++;
+	}
+	
+	if (question46 == "") {
+		correct++;
+	}
+	
+	if (question47 == "") {
+		correct++;
+	}
+	
+	if (question48 == "") {
+		correct++;
+	}
+	
+	if (question49 == "") {
+		correct++;
+	}
+	
+	if (question50 == "") {
+		correct++;
+	}
+	
+	if (question51 == "") {
+		correct++;
+	}
+	
+	if (question52 == "") {
+		correct++;
+	}
+	
+	if (question53 == "") {
+		correct++;
+	}
+	
+	if (question54 == "") {
+		correct++;
+	}
+	
+	if (question55 == "") {
+		correct++;
+	}
+	
+	if (question56 == "") {
+		correct++;
+	}
+	
+	if (question57 == "") {
+		correct++;
+	}
+	
+	document.getElementById("after_submit").style.visibility = "visible";
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct!";
+	
+}
